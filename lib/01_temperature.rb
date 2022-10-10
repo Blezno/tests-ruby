b04150825798bc49f88081822cecc0ea734960cc
blezno <gonzaleznovoa.b@protonmail.com>
2022-10-10 18:19:14 +0200

    first and second exercises completed

---
+++ lib/01_temperature.rb
@@ -0,0 +1,7 @@
+def ftoc(temp)
+    (temp - 32)*(5/9.0)
+end 
+
+def ctof(temps)
+    (temps * (9.0/5)) + 32
+end
---
+++ lib/03_basics.rb
@@ -0,0 +1,19 @@
+def who_is_bigger(a, b, c)
+    if a == nil || b == nil || c == nil
+        return "nil detected"
+    elsif a > b && a > c
+        return "a is bigger"
+    elsif b > c && b > a
+        return "b is bigger"
+    else c > a && c > b
+        return "c is bigger"
+    end
+end 
+
+def reverse_upcase_noLTA(strings)
+    strings.reverse.upcase!.delete("LTA")
+end
+
+def array_42(array)
+    
+end
